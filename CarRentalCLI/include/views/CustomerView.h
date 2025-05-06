529e9db37c77eefff81c0b5639f255fdbabcbe90
Peter Vatnar Andersson <peter@70x7.net>
2025-05-06 16:57:01 +0200

    Added documentation

--- CarRentalCLI/include/views/CustomerView.h
+++ CarRentalCLI/include/views/CustomerView.h
@@ -6,7 +6,29 @@
 
 class CustomerView {
 public:
+    /**
+ * @brief Draws a customer menu from which a user can decide to add edit or remove customers
+ */
     CustomerView();
+
+    /**
+ * @brief Prompts the user until a valid name is entered
+ * @param name Reference to input name string
+ */
+    void getName(std::string &name);
+
+    /**
+ * @brief Prompts the user until a valid telephone number is entered
+ * @param tel Reference to input tel string
+ */
+    void getTel(std::string &tel);
+
+    /**
+ * @brief Prompts the user until a valid email is entered
+ * @param email Reference to input email string
+ */
+    void getEmail(std::string &email);
+
 protected:
     std::string customerMenu = R"MENU(
 ==================================
@@ -20,6 +42,10 @@ protected:
 Please enter your choice:
 )MENU";
     CustomerController customerController;
+
+    /**
+ * @brief Prompts user for information about a new customer and dispatches it to controller
+ */
     void addCustomer();
     void editCustomer();
     void removeCustomer();
