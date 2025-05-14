3fe823f412b2c1a1f201f1f03bef8c22696a173b
Emanuel Rønningsland <emanuelr@uia.no>
2025-05-14 14:44:34 +0200

    Small design changes

--- CarRentalCLI/include/controllers/CustomerController.h
+++ CarRentalCLI/include/controllers/CustomerController.h
@@ -13,7 +13,9 @@ public:
     bool addCustomer(const std::string& name, const std::string& tel, const std::string& email);
     bool editCustomer(int id, std::string& name, std::string& tel, std::string& email);
     bool removeCustomer(int id);
+
     std::optional<CustomerModel> getCustomerByID(int id);
+
     int countCustomers();
 
     std::vector<CustomerModel> searchCustomer(const std::string& searchPhrase);
